99f514808fdc7c7dd67a8145fceb8a9bb3a618e0
SabrinaGiancaspro <sabrina.giancaspro13@gmail.com>
2023-11-02 15:06:25 +0100

    add mails in html

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-for (let i = 0; i < 10; i++){
+for (let i = 0; i < 10; i++) {
   axios
-  .get('https://flynn.boolean.careers/exercises/api/random/mail')
-  .then((res) => {
-  console.log(res.data.response)
-  })
+    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((res) => {
+      console.log(res.data.response);
+      const newEmail = res.data.response;
+      listDomElement.innerHTML += `<li>${newEmail}</li>`;
+    });
 }
 
-
+const listDomElement = document.querySelector('.email');
